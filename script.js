e76a95969a50f7d0b2cae5de08b4f3011e894c05
xSayfx <166869934+xSayfx@users.noreply.github.com>
2024-10-17 05:03:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ project3: {
       info: "الطالب : سيف اسماعيل دياب | الفصل : 2/3 | التاريخ : 2024/10/13",
       images: ["https://xsayfx.github.io/محاليل_قاعدية_1.png", "https://xsayfx.github.io/محاليل_قاعدية_2.png"],
       download: "https://xsayfx.github.io/‪مشروع رقمية.xlsx",
-},
-};
+  },
+ };
 
 let currentProject = '';
 let currentImageIndex = 0;
