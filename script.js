f01c70f1a35ac70f1be222ccc3af57a3fbc7dff3
xSayfx <166869934+xSayfx@users.noreply.github.com>
2024-10-18 21:16:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const projects = {
         subject: "تطبيق يعمل على الكمبيوتر | يجب تنزيل المشروع",
         info: "الطالب : سيف اسماعيل دياب | الفصل : 2/3 | التاريخ : 2024/10/18",
         images: ["https://xsayfx.github.io/calcmain.png"],
-        download: "https://xsayfx.github.io/CalcBySayf.zip",
+        download: "https://xsayfx.github.io/Calculator%20By%20Sayfx.zip",
     },
 };
 
