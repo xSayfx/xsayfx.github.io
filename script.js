5aad38b354746786346e161e1b06b7991b7d4590
xSayfx <166869934+xSayfx@users.noreply.github.com>
2024-10-19 20:31:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const projects = {
     project2: {
 //title
         info: "الطالب : سيف اسماعيل دياب | الفصل : 2/3 | التاريخ : 2024/10/13",
-        images: ["https://xsayfx.github.io/Ai_1.png", "https://xsayfx.github.io/Ai_2.png"],
+        images: ["https://xsayfx.github.io/AiNew_1.png", "https://xsayfx.github.io/AiNew_2.png"],
         download: "https://xsayfx.github.io/Ai.zip",
     },
     project3: {
